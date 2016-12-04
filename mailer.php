ef923cc26c876fe4d0095bb3b792703d6e990d3e
Suparna Chowdhury <kamela@metrodesk.com.bd>
2016-12-04 13:24:45 +0600

    Update mailer.php

--- mailer.php
+++ mailer.php
@@ -2,14 +2,14 @@
 require 'vendor/autoload.php';
 use \Uploadcare;
 
-$sendgrid = new SendGrid('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');// sendgrid api key
-$api = new Uploadcare\Api('489c6caa4162eb0092ef', '283eccde9c4d80374f18');// uploadcare api key
+$sendgrid = new SendGrid('sendgrid api key');// sendgrid api key
+$api = new Uploadcare\Api(' uploadcare api key', ' uploadcare secret key');// uploadcare api key
 
 $mail = new SendGrid\Email();// send email to Metrodesk
 $mail2 = new SendGrid\Email();// send confirmation email to client
 
-$name = "Metrodesk";// Sender name
-$email = "kamela@metrodesk.com.bd";// Sender Email
+$name = "example";// Sender name
+$email = "example@example.com";// Sender Email
 
 $fileurl = $_POST['uploader'];//get files URL
 $wlayer = $_POST['white-layer'];//get white layer input
@@ -35,14 +35,14 @@ $msg = "Order Id: $token \n$total images were uploaded by user. Please see instr
 for($i = 0, $w = 0, $a = 0, $b = 0, $c = 0, $l = 0, $tone = 0, $cc = 0,$mm = 0,$yy = 0,$kk = 0; $i < count($files) || $w <count($wlayer) || $a <count($alayer) || $b <count($blayer) || $c <count($clayer) || $l <count($llayer) || $tone <count($pantonev) || $cc <count($cmykc) || $mm <count($cmykm) || $yy <count($cmyky) || $kk <count($cmykk); $i++, $w++, $a++, $b++, $c++, $l++, $tone++,$cc++, $mm++,$yy++,$kk++) {
     	$msg.= "Download Link: ".$files[$i]->getUrl(). "\nWhite Layer: ".$wlayer[$w]."PX  |  Adhesive Layer: ".$alayer[$a]."PX  |  Blocking Layer: ".$blayer[$b]."PX  |  Clear Layer: ".$clayer[$c]."PX\nOutline Size: ".$llayer[$l]."PX  |  Pantone Code: ".$pantonev[$tone]."  |  CMYK C: ".$cmykc[$cc]." M: ".$cmykm[$mm]." Y: ".$cmyky[$yy]." K: ".$cmykk[$kk]."\n\n";
     }
-$msg .= "Kind Regards \nMetrodesk \nVisit: www.metrodesk.com.bd \nSkype: metrodeskbd[24 hours] \nEmail: support@metrodesk.com.bd";
+$msg .= "Kind Regards \nXYZ \nVisit: www.example.com \nSkype: XYZ[24 hours] \nEmail: support@example.com";
 // Order  Reciever
-$recipient ="kamela@metrodesk.com.bd";
+$recipient ="xyz@example.com";
 $subject = "New Order by DST - $token";
 // Confirmation reciever
-$confirm_recipient ="kamela@metrodesk.com.bd";
-$confirm_subject = "Thank you. Your Order: $token has been submitted to Metrodesk. ";
-$confirm_msg = "Order Id: $token \n\nNumber of images submitted: $total.\n\nThank you for your order. We will notify you when your order is ready.\n\nFor any support you can reply to this email or call us at 00880 1741589509.\n\nKind Regards \nMetrodesk \nVisit: www.metrodesk.com.bd \nSkype: metrodeskbd[24 hours] \nEmail: support@metrodesk.com.bd";
+$confirm_recipient ="xyz@example.com";
+$confirm_subject = "Thank you. Your Order: $token has been submitted to XYZ. ";
+$confirm_msg = "Order Id: $token \n\nNumber of images submitted: $total.\n\nThank you for your order. We will notify you when your order is ready.\n\nFor any support you can reply to this email or call us at 123456789.\n\nKind Regards \nXYZ \nVisit: www.example.com \nSkype: XYZ[24 hours] \nEmail: support@example.com";
 
 	// $mail:Send Order to Metrodesk
 	$mail->
@@ -63,7 +63,7 @@ $confirm_msg = "Order Id: $token \n\nNumber of images submitted: $total.\n\nThan
 		exit(header('Location: thank-you.html'));
 	}
 	else{
-		echo "Order submission failed. Please check for error or report to support@metrodesk.com.bd for Support.";
+		echo "Order submission failed. Please check for error or report to support@example.com for Support.";
 	}
 	
 ?>
